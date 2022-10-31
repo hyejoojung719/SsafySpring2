4271c5386c7677773a172458460f85a965d4cd5e
정혜주 <fixup719@gmail.com>
2022-10-31 17:51:26 +0900

    1031

---
+++ board6_springboot/src/main/java/com/ssafy/config/WebMvcConfiguration.java
@@ -0,0 +1,59 @@
+package com.ssafy.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import com.ssafy.interceptor.ConfirmInterceptor;
+
+@Configuration
+@EnableAspectJAutoProxy // aop 설정 
+@MapperScan(basePackages = { "com.ssafy.**.mapper" })	// mybatis 설정 
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+	// 인터셉터 mapping path 설정과 같음
+	private final List<String> patterns = Arrays.asList("/board/*", "/admin", "/user/list");
+
+	@Autowired	// 먼저 ConfirmInterceptor에 @Component 추가(컴포넌트 스캔 대상이 되어야 하므로) 
+	private ConfirmInterceptor confirmInterceptor;
+
+	private final String uploadFilePath;
+
+	public WebMvcConfiguration(@Value("${file.path.upload-files}") String uploadFilePath) {
+		this.uploadFilePath = uploadFilePath;
+	}
+
+	@Override	// 특정 url, 메소드로만 접근하고 싶을 떄
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*")
+//			.allowedOrigins("http://localhost:8080", "http://localhost:8081")
+				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+						HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+						HttpMethod.PATCH.name())
+//				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+				.maxAge(1800);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(confirmInterceptor).addPathPatterns(patterns);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/upload/file/**").addResourceLocations("file:///" + uploadFilePath + "/")
+				.setCachePeriod(3600).resourceChain(true).addResolver(new PathResourceResolver());
+	}
+
+}
