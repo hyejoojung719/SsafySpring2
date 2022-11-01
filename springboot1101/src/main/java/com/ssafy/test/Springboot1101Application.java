aca5d0665fde1c7dce97624cfdb5afe66c60a618
정혜주 <fixup719@gmail.com>
2022-11-01 17:57:36 +0900

    1101

---
+++ springboot1101/src/main/java/com/ssafy/test/Springboot1101Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1101Application.class, args);
+	}
+
+}
